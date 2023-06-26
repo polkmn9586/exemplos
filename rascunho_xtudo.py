fc9f3d410b37475af09047e1c4a8255f6d043ebd
polkmn9586 <mercadofera1@gmail.com>
2023-06-26 00:04:37 -0300

    26/06
    modificações

--- rascunho_xtudo.py
+++ rascunho_xtudo.py
@@ -23,21 +23,29 @@ bot=telebot.TeleBot("6218006765:AAF309hpXCmaU1r9P41zKiNK1L8gaqTMyqI")
 with open('loja.json') as file:
     loja_data = json.load(file)
 
+with open("itens.json") as file:
+    itens= json.load(file)
 
-categoria={}
+
+
+
 
 #---------------------------criar funções com as categorias produtos e itens--------------------------------
 
 def lista_categorias():
     categorias=loja_data.keys()
     return categorias
-def lista_produtos(categoria):
-    produtos=loja_data.get(categoria, {})
-    return produtos.keys()
+def lista_produtos(categoria,chat_id):
+    produtos={chat_id:loja_data.get(categoria,{}).keys()}
+
+    return produtos[chat_id]
 
 def lista_itens(categoria,produto):
     itens=loja_data[categoria][produto].get("Itens",[])
     return itens
+
+categorias=lista_categorias()
+
 
 #---------------------------Fim da criação das funções com as categorias produtos e itens--------------------------------
 
@@ -46,29 +54,25 @@ def lista_itens(categoria,produto):
 def inicio(message):
     mensagem= message.message_id
     chat=message.chat.id
+
+    markup= telebot.types.InlineKeyboardMarkup(row_width=1)
+    botao=telebot.types.InlineKeyboardButton("Fazer um pedido",callback_data="pedido")
+    markup.add(botao)
+    bot.send_message(message.chat.id, " Seja bem vindo a nossa loja",reply_markup=markup)
     for x in range(mensagem,mensagem-5,-1):
        try:
         bot.delete_message(chat,x)
        except:
            pass
-    markup= telebot.types.InlineKeyboardMarkup(row_width=1)
-    botao=telebot.types.InlineKeyboardButton("Fazer um pedido",callback_data="pedido")
-    markup.add(botao)
-    bot.send_message(message.chat.id, " Seja bem vindo a nossa loja",reply_markup=markup)
-
 
 #-------------------------------Escolha da categoria em que está o produto-----------------------------------------------
-categorias=lista_categorias()
+
 @bot.callback_query_handler(func= lambda call: call.data == "pedido")
 def escolha_categoria(call):
 
     mensagem = call.message.message_id
     chat = call.message.chat.id
-    for x in range(mensagem, mensagem - 5, -1):
-        try:
-            bot.delete_message(chat, x)
-        except:
-            pass
+
 
 
     markup=telebot.types.InlineKeyboardMarkup(row_width=1)
@@ -76,23 +80,104 @@ def escolha_categoria(call):
      botao=telebot.types.InlineKeyboardButton(x,callback_data=x)
      markup.add(botao)
     bot.send_message(chat,"Escolha a categoria do produto", reply_markup=markup )
+    for x in range(mensagem, mensagem - 5, -1):
+        try:
+            bot.delete_message(chat, x)
+        except:
+            pass
 
 
+categoria_escolhida={} # dicionário criado com a categoria escolhida pelo cliente
+produtos_da_categoria={} # dicionario criado com o id do cliente mais a lista de produtos da categoria escolhida
+
+# Aqui ocorrerá a escolha do produto
 @bot.callback_query_handler(func= lambda call: call.data in categorias)
 def escolha_produtos(call):
+    global categoria_escolhida ,produtos_da_categoria
     mensagem = call.message.message_id
     chat = call.message.chat.id
-    produtos=lista_produtos(call.data)
+
+    categoria_escolhida[chat]=call.data
+    produtos_da_categoria=lista_produtos(call.data,chat)
+
+    markup = telebot.types.InlineKeyboardMarkup(row_width=1)
+    for x in produtos_da_categoria:
+        botao = telebot.types.InlineKeyboardButton(x, callback_data=x)
+        markup.add(botao)
+    bot.send_message(chat, "Escolha o produto", reply_markup=markup)
+    for x in range(mensagem, mensagem - 5, -1):
+        try:
+            bot.delete_message(chat, x)
+        except:
+            pass
+
+dicionario_itens_produto={} # listará os itens do produto  escolhidos pelo cliene
+lista_itens_produto1=[] # lista dos itens escolhidos
+
+
+# Aqui , com o produto já escolhido ocorrerá a escolaha de seus itens
+@bot.callback_query_handler(func=lambda call: call.data in produtos_da_categoria)
+def escolha_itens(call):
+    global lista_produto , dicionario_itens_produto
+
+    mensagem = call.message.message_id
+    chat = call.message.chat.id
+
+    dicionario_itens_produto[chat]=list() # aqui eu informo que ele recebera uma lista
+
+
+    lista=lista_itens(categoria_escolhida[chat], call.data) # cria lista com os itens do produto escolhido
+    markup = telebot.types.InlineKeyboardMarkup(row_width=1) # cria o teclado dos itens
+    for x in lista: # for de criação do teclado
+        botao = telebot.types.InlineKeyboardButton(x, callback_data=x)
+        markup.add(botao)
+    lista_mensagem=" - ".join(lista)
+    bot.send_message(chat, f"Produto escolhido : {call.data}\nPossiveis acompanhamentos: {lista_mensagem} ")
+    bot.send_message(chat, "Escolha os Itens que deseja remover do lanche ", reply_markup=markup) # mensagem que trará o teclado
 
     for x in range(mensagem, mensagem - 5, -1):
         try:
             bot.delete_message(chat, x)
         except:
             pass
-    markup = telebot.types.InlineKeyboardMarkup(row_width=1)
-    for x in produtos:
-        botao = telebot.types.InlineKeyboardButton(x, callback_data=x)
-        markup.add(botao)
-    bot.send_message(chat, "Escolha a categoria do produto", reply_markup=markup)
+
+
+
+ # para isso farei um botão de prosseguir que me levará a a proxima tela , lembrar de colocar a mensagens dos itens escolhidos
+    # farei a lógica para criar a lista com todos  os itens , variavel normal
+    # depois criarei uma teclado com essas opções
+    # esse teclado terá opção de proseguir com o pedido
+
+
+
+
+mensagem_acumulada_itens=int()
+@bot.callback_query_handler(func=lambda call: call.data in itens)
+def itens_escolha(call):
+    global  dicionario_itens_produto, mensagem_acumulada_itens
+
+    mensagem = call.message.message_id
+    chat = call.message.chat.id
+
+    if mensagem_acumulada_itens: # deleta a mensagem e insere None no acumulador
+        bot.delete_message(chat,mensagem_acumulada_itens)
+        mensagem_acumulada_itens=None
+
+
+    if call.data in dicionario_itens_produto[chat]: # lógica para acrescentar itens
+        dicionario_itens_produto[chat].remove(call.data)
+    else:
+        dicionario_itens_produto[chat].append(call.data)
+    lista= " - ".join(dicionario_itens_produto[chat])
+
+    markup= telebot.types.InlineKeyboardMarkup(row_width=1)
+    botão=telebot.types.InlineKeyboardButton("Prosseguir",callback_data="pros")
+    markup.add(botão)
+    mensagem=bot.send_message(chat,f"Itens adicionados : {lista}\n Deseja prosseguir"
+                                   f" com o pedido? Caso ainda queira é só retirar mais itens",reply_markup=markup) # dispara a mensagem e atribui  informações da mensagem
+    mensagem_acumulada_itens=mensagem.message_id # recebe seu id
+
+
+
 
 bot.polling()
